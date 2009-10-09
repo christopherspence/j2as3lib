882d0c97946d01304180e8bcdc9f715284b7db38
ghaladen <ghaladen@52710764-b50e-11de-88a7-9da883da0866>
2009-10-09 21:29:34 +0000

    More classes

---
+++ trunk/ j2as3lib/j2as3lib-away3dlite/src/away3dlite/animators/Frame.java
@@ -0,0 +1,30 @@
+package away3dlite.animators;
+
+import flash.base.Vector;
+
+public class Frame {
+	
+	/**
+	 * The name of the frame.
+	 */
+	public String name;
+	
+	/**
+	 * The array of vertices contained inside the frame 
+	 */
+	public Vector vertices;
+	
+	/**
+	 * The array of vertices contained inside the frame. 
+	 * @param name The name of the frame.
+	 * @param vertices An array of Vertex objects.
+	 */
+	public Frame(String name, Vector vertices) {}
+	
+	/**
+	 * Returns a string representation of the Frame object
+	 */
+	public String toString() {
+		return "";
+	}
+}
