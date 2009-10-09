431dbc0d9820f7607243da4a2f0f089ada9e3fb9
ghaladen <ghaladen@52710764-b50e-11de-88a7-9da883da0866>
2009-10-09 21:29:34 +0000

    More classes

---  j2as3lib/j2as3lib-away3dlite/src/away3dlite/animators/MovieMesh.java
+++  j2as3lib/j2as3lib-away3dlite/src/away3dlite/animators/MovieMesh.java
@@ -1,7 +1,41 @@
 package away3dlite.animators;
 
+import flash.base.Vector;
 import away3dlite.core.base.Mesh;
 
+/**
+ * Animates a series of Frame objects in sequence in a mesh. 
+ */
 public class MovieMesh extends Mesh {
 
+	// CONSTANTS
+	public final int ANIM_LOOP = 2;
+	
+	public final int ANIM_NORMAL = 1;
+	
+	public final int ANIM_STOP = 4;
+	
+	/**
+	 * Number of animation frames to display per second 
+	 */
+	public int fps = 24;
+	
+	/**
+	 * The array of frames that make up the animation sequence.
+	 */
+	public Vector frames;
+	
+	/**
+	 * Defines the current keyframe.
+	 */
+	public int keyframe;
+	
+	/**
+	 * Creates a new MovieMesh object that provides a "keyframe animation"/"vertex animation"/"mesh deformation" framework for subclass loaders.
+	 */
+	public MovieMesh() {}
+	
+	public void addFrame(Frame frame) {}
+	
+	public void loop(int begin, int end) {}
 }
